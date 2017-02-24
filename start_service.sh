6fcaffa2aca8c0406062fa6de2cf208996e6d342
Vitalii Kulanov <vitaliy@kulanov.org.ua>
2017-02-24 13:27:55 +0200

    Align parameters for docker run command in start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Jekyll service starting..."
-docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -e JEKYLL_ENV=production jekyll/jekyll:pages jekyll serve --watch --incremental
+docker run --rm -t -i -v "$PWD:/srv/jekyll" -p 4000:4000 jekyll/jekyll:pages jekyll serve --watch --incremental
